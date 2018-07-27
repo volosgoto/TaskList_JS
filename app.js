0b4209b39e010c03e5a8f426be8e8567bdd43e4c
volosgoto <volosgoto@yahoo.com>
2018-07-27 10:27:13 +0300

    add UI vars

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// alert('Hello Task list');
+// 
+
+let form = document.querySelector('#task-form');
+let taskList = document.querySelector('.collection');
+let clearBtn = document.querySelector('.cleart-tasks');
+let filter = document.querySelector('#filter');
